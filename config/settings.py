74896401872c0375876b4d8091aa4807e8836f54
keyleeu <minhk.lee21@gmail.com>
2021-04-14 21:54:08 +0900

    aws2

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "%rstbb6*sr2op#67*0vywwk0rxdlru#r15@#@&z%#!8^(69v11"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = bool(os.environ.get("DEBUG"))
 
 ALLOWED_HOSTS = []
 
@@ -88,12 +89,27 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+if DEBUG:
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
     }
-}
+
+else:
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "HOST": os.environ.get("RDS.HOST"),
+            "NAME": os.environ.get("RDS.NAME"),,
+            "USER": os.environ.get("RDS.USER"),,
+            "PASSWORD": os.environ.get("RDS.PASSWORD"),,
+            "PORT": "5432",
+        }
+    }
 
 
 # Password validation
