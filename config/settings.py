b607083c9c0b4e19d8ca04a55a9d27f88fa1cc6d
keyleeu <minhk.lee21@gmail.com>
2021-04-19 18:41:32 +0900

    419 aws13

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("DJANGO_SECRET", "1j9T$D7IJBMBimM$JMZ2T^8QLEIc6B6")
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG"))
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost"]
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG is False:
+if DEBUG:
 
     DATABASES = {
         "default": {
